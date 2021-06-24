202d77a6d84fda8f39a7ca9a47aa0de32b14e21d
Martin Amm <ma@adenin.com>
2021-06-24 15:40:39 +0100

    test

---
+++ develop.cmd
@@ -0,0 +1,1 @@
+yarn workspace digital-assistant gatsby develop
---
+++ setup.cmd
@@ -0,0 +1,3 @@
+git clone https://gitlab.com/adenin-team/platform-client-gatsby.git
+git clone https://github.com/grommet/grommet.git
+yarn
