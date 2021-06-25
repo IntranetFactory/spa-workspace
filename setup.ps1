8f0d7ae2c9f08581f4cc82f73dc17d728ac38191
olliejm <olliejm@protonmail.com>
2021-06-25 15:53:42 +0100

    workspace works with yarn link

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,15 @@
-git clone https://gitlab.com/adenin-team/platform-client-gatsby.git
+git clone https://github.com/grommet/grommet-designer
 git clone https://github.com/grommet/grommet.git
+git clone https://gitlab.com/adenin-team/platform-client-gatsby
 Set-Location .\grommet
 yarn install
+yarn run build
+Set-Location .\dist
 yarn link
+Set-Location ..\..\grommet-designer
+yarn install
+yarn link grommet
 Set-Location ..\platform-client-gatsby
+git checkout development
 yarn install
 yarn link grommet
